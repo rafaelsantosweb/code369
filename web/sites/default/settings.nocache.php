790006c3ba7845b8e8da3a8a67902173ad7c99ad
Alessandro Feijó <desenvolvimento9@cityconnect.com.br>
2023-05-29 15:00:11 -0300

    Env setup

---
+++ web/sites/default/settings.nocache.php
@@ -0,0 +1,16 @@
+<?php
+
+// Disable cache
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/dev.services.yml';
+$settings['container_yamls'][] = __DIR__ . '/services.local.yml';
+$config['system.performance']['css']['preprocess'] = false;
+$config['system.performance']['js']['preprocess'] = false;
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+
+$config['config_split.config_split.local']['status'] = true;
+$config['config_split.config_split.develop']['status'] = true;
+
+$settings['config_exclude_modules'][] = 'devel';
+$settings['config_exclude_modules'][] = 'stage_file_proxy';
